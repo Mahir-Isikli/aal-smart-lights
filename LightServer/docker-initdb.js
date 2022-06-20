0d84051a02ccb4b3aef5a64f7094685f9e85169f
William Knöpp <ya5nx4rdjc@privaterelay.appleid.com>
2022-06-21 00:27:02 +0200

    Added mock-data for DB to docker-initdb.js

--- LightServer/docker-initdb.js
+++ LightServer/docker-initdb.js
@@ -1,1 +1,43 @@
-printjson('Hello World')
+conn = new Mongo();
+db = connect("mongodb:27020/LightServer");
+
+db.lampconfigs.bulkWrite( [
+    { insertOne : { "document" : { "id": "lukeOn",  "lampId": "LukeRoberts",  "color": "FFFFFF",  "intensity": 100,  "turnedOn": true} } },
+    { insertOne : { "document" : {
+                "id": "stripOn",
+                "lampId": "LightStrip",
+                "color": "00A0FF",
+                "intensity": 100,
+                "turnedOn": true
+            } } }
+])
+
+db.lamps.bulkWrite( [
+    { insertOne : { "document" : {  "id": "LukeRoberts",  "hasColor": true,  "hasIntensity": true,  "type": "bluetooth",  "ip": "4.3.2.1"} } },
+    { insertOne : { "document" : {  "id": "LightStrip",  "hasColor": true,  "hasIntensity": true,  "type": "wled",  "ip": "4.3.2.1"} } }
+])
+
+db.lampconfigs.lightevents( [
+    { insertOne : { "document" : {
+                "id": "sitDown",
+                "scenes": [
+                    "bright_lights",
+                    "strip1"
+                ]
+            } } }
+])
+
+db.lightscenes.bulkWrite( [
+    { insertOne : { "document" : {
+                "id": "bright_lights",
+                "lampConfigs": [
+                    "lukeOn"
+                ]
+            } } },
+    { insertOne : { "document" : {
+                "id": "strip1",
+                "lampConfigs": [
+                    "stripOn"
+                ]
+            } } }
+])
