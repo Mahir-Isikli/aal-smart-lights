566ac245f5113fc3926c247af2e0cae70433ee06
William Knöpp <ya5nx4rdjc@privaterelay.appleid.com>
2022-06-21 00:34:36 +0200

    added comments to docker-initdb.js

--- LightServer/docker-initdb.js
+++ LightServer/docker-initdb.js
@@ -1,6 +1,7 @@
 conn = new Mongo();
 db = connect("mongodb:27020/LightServer");
 
+// add lampconfigs
 db.lampconfigs.bulkWrite( [
     { insertOne : { "document" : { "id": "lukeOn",  "lampId": "LukeRoberts",  "color": "FFFFFF",  "intensity": 100,  "turnedOn": true} } },
     { insertOne : { "document" : {
@@ -12,11 +13,13 @@ db.lampconfigs.bulkWrite( [
             } } }
 ])
 
+// add lamps
 db.lamps.bulkWrite( [
     { insertOne : { "document" : {  "id": "LukeRoberts",  "hasColor": true,  "hasIntensity": true,  "type": "bluetooth",  "ip": "4.3.2.1"} } },
     { insertOne : { "document" : {  "id": "LightStrip",  "hasColor": true,  "hasIntensity": true,  "type": "wled",  "ip": "4.3.2.1"} } }
 ])
 
+// add events
 db.lampconfigs.lightevents( [
     { insertOne : { "document" : {
                 "id": "sitDown",
@@ -27,6 +30,7 @@ db.lampconfigs.lightevents( [
             } } }
 ])
 
+// add scenes
 db.lightscenes.bulkWrite( [
     { insertOne : { "document" : {
                 "id": "bright_lights",
@@ -41,4 +45,3 @@ db.lightscenes.bulkWrite( [
                 ]
             } } }
 ])
-//
